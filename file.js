9f74253880f30345e9464f50a843abb3228ed6a9
Manuradha Singh <your_email@example.com>
2026-01-22 14:56:52 +0530

    [manu]add: class practice in folder two subfolder are added and in on of sub folder taxt file added

---
+++ file.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const path =require("path");
+const mainfolder = "./MainFolder";
+const a= path.join(mainfolder,"a");
+const b = path.join(mainfolder, "b");
+const filePath = path.join(b,"hello.txt");
+fs.mkdirSync(mainfolder, { recursive: true});
+fs.mkdirSync(a, {recursive: true});
+fs.mkdirSync(b, { recursive: true });
+fs.writeFileSync(filePath, "hello worls again");
+console.log("folder ended");
